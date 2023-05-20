3e4ed3636495dd6a0847e86f8221be8b805aa7a0
Fabio <fabio89@gmail.com>
2023-05-20 17:45:04 +0200

    .

--- hello-world/HelloWorld.py
+++ hello-world/HelloWorld.py
@@ -1,1 +1,6 @@
-print ("Hello World")
+# print ("Hello World")
+age = 34
+name = 'Fabio'
+city = 'Bari'
+print(f'My name is {name} and i am {age} years old and I was born in {city}')
+print('What\'s your name?')
